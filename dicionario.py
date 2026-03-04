ea2e52c1ee311a1c45388aa7986d7aab593b902d
Mery Lully Ferreira de Brito <915781@senacminas.edu.br>
2026-03-04 08:57:58 -0300

    musicapython

---
+++ dicionario.py
@@ -0,0 +1,9 @@
+Aluno={
+    "Nome" : "Mery_lully",
+    "Sobrenome": "brito",
+    "idade": 30,
+    "Animal de estimacao" : "cachorro",
+    "Cor preferida" : "rosa",
+    "escolaridade" : "ensino medio"
+            }
+print(Aluno)
